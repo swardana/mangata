49f5edaa2b08754b56d9786b9d980eb9e2f70435
Sukma Wardana <swardana@tutanota.com>
2021-09-08 12:48:58 +0700

    Add mangata-metro module descriptor

---
+++ mangata-metro/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+/**
+ * The mangata-metro module descriptor.
+ *
+ * @moduleGraph
+ */
+module com.swardana.mangata.metro {
+
+    requires java.base;
+
+    requires javafx.base;
+    requires javafx.graphics;
+
+    requires com.swardana.mangata.base;
+
+    exports com.swardana.mangata.metro;
+
+}
