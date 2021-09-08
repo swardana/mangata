edc73451d4677f53d904f676a50642d2aed572fc
Sukma Wardana <swardana@tutanota.com>
2021-09-08 13:56:18 +0700

    Add mangata-material module

---
+++ mangata-material/src/main/java/com/swardana/mangata/material/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * JavaFX UI Control for Material Design Icon Font version 4.
+ *
+ * @see <a href="https://github.com/google/material-design-icons">Material Icon</a>
+ */
+package com.swardana.mangata.material;
