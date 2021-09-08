402ff621c6893813d9fd007e4b3f3e0dfc62d69e
Sukma Wardana <swardana@tutanota.com>
2021-09-08 11:07:36 +0700

    Add mangata-base module descriptor

---
+++ mangata-base/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * The mangata-base module descriptor.
+ *
+ * @moduleGraph
+ */
+module com.swardana.mangata.base {
+
+    requires java.base;
+
+    requires javafx.base;
+    requires javafx.graphics;
+
+    exports com.swardana.mangata.base;
+
+}
