e1eb245ac1fbfe0c12b5b598fb3d9f1344776266
Sukma Wardana <swardana@tutanota.com>
2021-09-08 14:03:25 +0700

    Add mangata-material module descriptor

---
+++ mangata-material/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+/**
+ * The mangata-material module descriptor.
+ *
+ * @moduleGraph
+ */
+module com.swardana.mangata.material {
+
+    requires java.base;
+
+    requires javafx.base;
+    requires javafx.graphics;
+
+    requires com.swardana.mangata.base;
+
+    exports com.swardana.mangata.material;
+
+}
