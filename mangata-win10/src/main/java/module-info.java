27f60a8de25c9c493314c46f5a6600b777266122
swardana <swardana@tutanota.com>
2021-11-09 17:11:10 +0700

    Add mangata-win10 module descriptor

---
+++ mangata-win10/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+/**
+ * The mangata-win10 module descriptor.
+ *
+ * @moduleGraph
+ */
+module com.swardana.mangata.win10 {
+
+    requires java.base;
+
+    requires javafx.base;
+    requires javafx.graphics;
+
+    requires com.swardana.mangata.base;
+
+    exports com.swardana.mangata.win10;
+
+}
