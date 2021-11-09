39ab3e0db62266c275f086c1823c08abafba0d75
swardana <swardana@tutanota.com>
2021-11-09 17:04:15 +0700

    Add mangata-win10 module

---
+++ mangata-win10/src/main/java/com/swardana/mangata/win10/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * JavaFX UI Control for Icons8 Windows 10 Icon Font version 1.
+ *
+ * @see <a href="https://github.com/icons8/windows-10-icons">Icons8 Windows 10 Icon</a>
+ */
+package com.swardana.mangata.win10;
